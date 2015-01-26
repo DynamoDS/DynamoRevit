1d00947ca02045be0a41e80cac67b50a28dc0194
Peter Boyer <peter.b.boyer@gmail.com>
2015-01-26 16:56:04 -0500

    Fix merge conflicts

--- src/DynamoRevit/DynamoRevit.cs
+++ src/DynamoRevit/DynamoRevit.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 
 using Dynamo.Applications;
-
+using Greg.AuthProviders;
 using RevitServices.Elements;
 
 #region
@@ -112,6 +112,7 @@ namespace Dynamo.Applications
 
                 TryOpenWorkspaceInCommandData(extCommandData);
                 SubscribeApplicationEvents(extCommandData);
+
                 // Disable the Dynamo button to prevent a re-run
                 DynamoRevitApp.DynamoButton.Enabled = false;
             }
@@ -158,9 +159,9 @@ namespace Dynamo.Applications
                     Preferences = prefs,
                     DynamoCorePath = corePath,
                     Context = GetRevitContext(commandData),
-                    SchedulerThread = new RevitSchedulerThread(commandData.Application)
+                    SchedulerThread = new RevitSchedulerThread(commandData.Application),
+                    AuthProvider = new RevitOxygenProvider(new DispatcherSynchronizationContext(Dispatcher.CurrentDispatcher))
                 });
-
         }
 
         private static DynamoViewModel InitializeCoreViewModel(RevitDynamoModel revitDynamoModel)
@@ -175,9 +176,6 @@ namespace Dynamo.Applications
                     WatchHandler =
                         new RevitWatchHandler(vizManager, revitDynamoModel.PreferenceSettings)
                 });
-
-            revitDynamoModel.PackageManagerClient.RequestAuthentication +=
-                 SingleSignOnManager.RegisterSingleSignOn;
 
             revitDynamoModel.ShutdownStarted += (drm) =>
             {
@@ -198,8 +196,6 @@ namespace Dynamo.Applications
 
             dynamoView.Dispatcher.UnhandledException += Dispatcher_UnhandledException;
             dynamoView.Closed += OnDynamoViewClosed;
-
-            SingleSignOnManager.UIDispatcher = dynamoView.Dispatcher;
 
             return dynamoView;
         }
@@ -244,6 +240,7 @@ namespace Dynamo.Applications
 
         public static void InitializeAssemblies()
         {
+            RevitAssemblyLoader.LoadAll();
             AppDomain.CurrentDomain.AssemblyResolve +=
                 Analyze.Render.AssemblyHelper.ResolveAssemblies;
         }
