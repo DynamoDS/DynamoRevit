e861cab808db1f4524ba198e2fd35cba380c6c90
kiki-zhang <xiaojie.zhang@autodesk.com>
2015-02-02 17:52:03 +0800

    fix compile error

--- test/Libraries/RevitIntegrationTests/SelectionTests.cs
+++ test/Libraries/RevitIntegrationTests/SelectionTests.cs
@@ -21,6 +21,7 @@ using RevitServices.Transactions;
 using RTF.Framework;
 
 using ReferencePoint = Revit.Elements.ReferencePoint;
+using Surface = Autodesk.DesignScript.Geometry.Surface;
 
 namespace RevitSystemTests
 {
@@ -47,7 +48,7 @@ namespace RevitSystemTests
             //in the node's items source
             var fec = new FilteredElementCollector(DocumentManager.Instance.CurrentUIDocument.Document);
             fec.OfClass(typeof(Family));
-            int count = fec.ToElements().Cast<Family>().Sum(f => f.Symbols.Cast<FamilySymbol>().Count());
+            int count = fec.ToElements().Cast<Family>().Sum(f => f.GetFamilySymbolIds().Count());
 
             var typeSelNode = (FamilyTypes)ViewModel.Model.CurrentWorkspace.Nodes.First();
             Assert.AreEqual(typeSelNode.Items.Count, count);
