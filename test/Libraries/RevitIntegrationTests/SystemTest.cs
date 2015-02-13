cbcd0807062f5b98e470de739eac9fc681d78b58
Ian Keough <ian.keough@autodesk.com>
2015-02-13 11:42:41 -0800

    Override test setup to load specific ASM

--- test/Libraries/RevitIntegrationTests/SystemTest.cs
+++ test/Libraries/RevitIntegrationTests/SystemTest.cs
@@ -6,10 +6,17 @@ using System.Linq;
 using System.Reflection;
 using System.Xml.Serialization;
 using Autodesk.Revit.DB;
+
+using Dynamo;
 using Dynamo.Models;
 using DynamoUtilities;
+
+using NUnit.Framework;
+
 using RevitServices.Persistence;
 using RevitTestServices;
+
+using TestServices;
 
 namespace RevitSystemTests
 {
@@ -122,6 +129,28 @@ namespace RevitSystemTests
         protected string defsPath;
         protected string emptyModelPath1;
         protected string emptyModelPath;
+
+        [SetUp]
+        public override void Setup()
+        {
+            AssemblyResolver.Setup();
+
+            SetupCore();
+
+            if (string.IsNullOrEmpty(workingDirectory))
+            {
+                workingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            }
+
+            DynamoPathManager.PreloadAsmVersion("221", DynamoPathManager.Instance);
+
+            CreateTemporaryFolder();
+
+            // Setup Temp PreferenceSetting Location for testing
+            PreferenceSettings.DynamoTestPath = Path.Combine(TempFolder, "UserPreferenceTest.xml");
+
+            StartDynamo();
+        }
 
         public override void SetupCore()
         {
