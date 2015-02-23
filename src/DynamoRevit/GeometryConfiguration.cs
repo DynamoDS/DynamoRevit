47911c74b946f7d69c42e429deab97fa56866db1
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-23 12:34:15 +0800

    Implemented Revit specific version of "GeometryConfiguration" class

---
+++ src/DynamoRevit/GeometryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dynamo.Interfaces;
+
+namespace Dynamo.Applications
+{
+    class GeometryConfiguration : IGeometryConfiguration
+    {
+        public bool PreloadShapeManager
+        {
+            get { return false; } // ASM is always preloaded in Revit.
+        }
+
+        public string ShapeManagerPath
+        {
+            // Returning 'false' for 'PreloadShapeManager' so this property
+            // will not be called, throw an exception if it's been called.
+            get { throw new NotImplementedException(); }
+        }
+
+        public LibraryVersion Version
+        {
+            // This is specific to current Revit version.
+            get { return LibraryVersion.Version219; }
+        }
+    }
+}
