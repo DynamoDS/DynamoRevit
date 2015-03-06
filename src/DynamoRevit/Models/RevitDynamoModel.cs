aff361b4086854a07ee5ca78adb3024370c2e663
Peter Boyer <peter.b.boyer@gmail.com>
2015-03-06 14:47:30 -0500

    Refactor

--- src/DynamoRevit/Models/RevitDynamoModel.cs
+++ src/DynamoRevit/Models/RevitDynamoModel.cs
@@ -424,7 +424,7 @@ namespace Dynamo.Applications.Models
         {
             foreach (var ws in Workspaces.OfType<HomeWorkspaceModel>())
             {
-                ws.MarkNodesAsModified(ws.Nodes);
+                ws.MarkNodesAsModifiedAndRequestRun(ws.Nodes);
                 
                 foreach (var node in ws.Nodes)
                 {
