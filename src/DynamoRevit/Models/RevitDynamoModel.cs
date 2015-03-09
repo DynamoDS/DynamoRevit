1a93eed77ce66b3f2e166f7d5304ffe070edd047
Ian Keough <ian.keough@autodesk.com>
2015-03-09 15:21:48 -0700

    Remove redundant cast.

--- src/DynamoRevit/Models/RevitDynamoModel.cs
+++ src/DynamoRevit/Models/RevitDynamoModel.cs
@@ -420,7 +420,7 @@ namespace Dynamo.Applications.Models
                 OnRevitContextUnavailable();
 
                 foreach (
-                    var ws in Workspaces.OfType<HomeWorkspaceModel>().Cast<HomeWorkspaceModel>())
+                    var ws in Workspaces.OfType<HomeWorkspaceModel>())
                 {
                     ws.RunSettings.RunEnabled = false;
                 }
