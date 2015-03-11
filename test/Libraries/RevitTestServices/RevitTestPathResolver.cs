5a93abbf8a1fb3ff5ab6fd8bb517fee7f01f1f6f
Benglin (Ben) Goh <benglin@outlook.com>
2015-03-11 16:19:20 +0800

    Implemented "RevitTestPathResolver" class

---
+++ test/Libraries/RevitTestServices/RevitTestPathResolver.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Dynamo.Interfaces;
+
+namespace RevitTestServices
+{
+    public class RevitTestPathResolver : IPathResolver
+    {
+        private readonly HashSet<string> additionalResolutionPaths;
+        private readonly HashSet<string> additionalNodeDirectories;
+        private readonly HashSet<string> preloadedLibraryPaths;
+
+        public RevitTestPathResolver()
+        {
+            additionalResolutionPaths = new HashSet<string>();
+            additionalNodeDirectories = new HashSet<string>();
+            preloadedLibraryPaths = new HashSet<string>();
+        }
+
+        public IEnumerable<string> AdditionalResolutionPaths
+        {
+            get { return additionalResolutionPaths; }
+        }
+
+        public IEnumerable<string> AdditionalNodeDirectories
+        {
+            get { return additionalNodeDirectories; }
+        }
+
+        public IEnumerable<string> PreloadedLibraryPaths
+        {
+            get { return preloadedLibraryPaths; }
+        }
+
+        public void AddNodeDirectory(string nodeDirectory)
+        {
+            if (!additionalNodeDirectories.Contains(nodeDirectory))
+                additionalNodeDirectories.Add(nodeDirectory);
+        }
+
+        public void AddResolutionPath(string resolutionPath)
+        {
+            if (!additionalResolutionPaths.Contains(resolutionPath))
+                additionalResolutionPaths.Add(resolutionPath);
+        }
+
+        public void AddPreloadLibraryPath(string preloadLibraryPath)
+        {
+            if (!preloadedLibraryPaths.Contains(preloadLibraryPath))
+                preloadedLibraryPaths.Add(preloadLibraryPath);
+        }
+    }
+}
