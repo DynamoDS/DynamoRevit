1aa256e093d33d6af877ec5a955970df70020b34
Ian Keough <ian.keough@autodesk.com>
2015-03-12 13:13:22 -0700

    Fix node element updating.

--- src/DynamoRevit/Models/RevitDynamoModel.cs
+++ src/DynamoRevit/Models/RevitDynamoModel.cs
@@ -591,7 +591,7 @@ namespace Dynamo.Applications.Models
             var nodes = ElementBinder.GetNodesFromElementIds(updatedIds, CurrentWorkspace, EngineController);
             foreach (var node in nodes)
             {
-                node.OnNodeModified(false);
+                node.OnNodeModified(true);
             }
         }
 
