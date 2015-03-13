0f019d003ac4b40ba6acf52714e4d6617860942c
Ian Keough <ian.keough@autodesk.com>
2015-03-12 23:52:06 -0700

    Check and bail.

--- src/DynamoRevit/Models/RevitDynamoModel.cs
+++ src/DynamoRevit/Models/RevitDynamoModel.cs
@@ -239,6 +239,11 @@ namespace Dynamo.Applications.Models
 
         private void UnsubscribeApplicationEvents(ExternalCommandData commandData)
         {
+            if (!hasRegisteredApplicationEvents)
+            {
+                return;
+            }
+
             commandData.Application.ViewActivating -= OnApplicationViewActivating;
             commandData.Application.ViewActivated -= OnApplicationViewActivated;
 
