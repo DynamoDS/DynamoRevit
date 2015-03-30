929a2b055ce2be8c8cdfdea5ef13839ad991b511
U-ADS\keyu <keyu@SINSGH038PRFK.ads.autodesk.com>
2015-03-30 16:42:34 +0800

    Change Category's OutputData's nickname instead of setting PortName

--- src/Libraries/RevitNodesUI/RevitDropDown.cs
+++ src/Libraries/RevitNodesUI/RevitDropDown.cs
@@ -410,8 +410,9 @@ namespace DSRevitNodesUI
     {
         public Categories()
         {
-            OutPorts[0].PortName = "Category";
+            OutPortData[0].NickName = "Category";
             OutPortData[0].ToolTipString = "The selected Category.";
+            OutPorts[0].SetPortData(OutPortData[0]);
         }
 
         public override void PopulateItems()
