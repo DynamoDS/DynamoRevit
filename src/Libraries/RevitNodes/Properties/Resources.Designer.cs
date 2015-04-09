396404d83663b1f35539e381f736fba187f154ea
Benglin (Ben) Goh <benglin@outlook.com>
2015-04-09 17:13:30 +0800

    Addressed review comments

--- src/Libraries/RevitNodes/Properties/Resources.Designer.cs
+++ src/Libraries/RevitNodes/Properties/Resources.Designer.cs
@@ -135,9 +135,9 @@ namespace Revit.Properties {
         /// <summary>
         ///   Looks up a localized string similar to The number of divisions must be greater than 2!.
         /// </summary>
-        internal static string NumberOfDivisionMustBeGreaterThan2 {
+        internal static string NumberOfDivisionsMustBeGreaterThan2 {
             get {
-                return ResourceManager.GetString("NumberOfDivisionMustBeGreaterThan2", resourceCulture);
+                return ResourceManager.GetString("NumberOfDivisionsMustBeGreaterThan2", resourceCulture);
             }
         }
         
