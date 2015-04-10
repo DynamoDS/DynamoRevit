c63cbfdf441813884945c60404b319cf697b1a3f
Randy-Ma <zzyymm01@gmail.com>
2015-04-10 10:23:38 +0800

    Fix MAGN-6710 Run CurveByPoints.byReferencePoints will crash Revit and Dynamo
    Conflicts:
    	test/Libraries/RevitIntegrationTests/BugTests.cs

--- src/Libraries/RevitNodes/Elements/CurveByPoints.cs
+++ src/Libraries/RevitNodes/Elements/CurveByPoints.cs
@@ -8,6 +8,8 @@ using DynamoServices;
 using RevitServices.Persistence;
 using RevitServices.Transactions;
 
+using Revit.GeometryConversion;
+
 namespace Revit.Elements
 {
     /// <summary>
@@ -20,16 +22,25 @@ namespace Revit.Elements
 
         private void InternalSetReferencePoints(ReferencePointArray pts)
         {
-            TransactionManager.Instance.EnsureInTransaction(Document);
-            ((Autodesk.Revit.DB.CurveByPoints) InternalCurveElement).SetPoints(pts);
-            TransactionManager.Instance.TransactionTaskDone();
+            var cbp = ((Autodesk.Revit.DB.CurveByPoints)InternalCurveElement) as Autodesk.Revit.DB.CurveByPoints;
+            var crvPnts = cbp.GetPoints();
+            if (!CurveUtils.PointArraysAreSame(crvPnts, pts))
+            {
+                TransactionManager.Instance.EnsureInTransaction(Document);
+                ((Autodesk.Revit.DB.CurveByPoints)InternalCurveElement).SetPoints(pts);
+                TransactionManager.Instance.TransactionTaskDone();
+            }
         }
 
         private void InternalSetIsReferenceLine(bool isReferenceLine)
         {
-            TransactionManager.Instance.EnsureInTransaction(Document);
-            ((Autodesk.Revit.DB.CurveByPoints) InternalCurveElement).IsReferenceLine = isReferenceLine;
-            TransactionManager.Instance.TransactionTaskDone();
+            var cbp = ((Autodesk.Revit.DB.CurveByPoints)InternalCurveElement) as Autodesk.Revit.DB.CurveByPoints;
+            if (cbp.IsReferenceLine != isReferenceLine)
+            {
+                TransactionManager.Instance.EnsureInTransaction(Document);
+                cbp.IsReferenceLine = isReferenceLine;
+                TransactionManager.Instance.TransactionTaskDone();
+            }
         }
 
         #endregion
@@ -79,7 +90,7 @@ namespace Revit.Elements
             {
                 InternalSetCurveElement(cbp);
                 InternalSetReferencePoints(refPtArr);
-                cbp.IsReferenceLine = isReferenceLine;
+                InternalSetIsReferenceLine(isReferenceLine);
                 return;
             }
 
