49e3149742120f984208893a1e97156ec2c0a23d
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-04-28 16:12:06 +0800

    revit test configuration

---
+++ test/Libraries/RevitIntegrationTests/WorkFlowTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+using NUnit.Framework;
+
+using RevitTestServices;
+
+using RTF.Framework;
+
+namespace RevitSystemTests
+{
+    [TestFixture]
+    public class WorkflowTests : RevitSystemTestBase
+    {
+        
+
+        [Test]
+        [TestModel(@".\Workflow\empty.rfa")]
+        public void Workflow_test01()
+        {
+            // Create Wall.ByCurveAndHeight
+            string samplePath = Path.Combine(workingDirectory, @".\Workflow\WorkFlow_test01.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+
+            RunCurrentModel();
+            
+            
+        }
+
+        
+    }
+}
