57e629d5c0a27998ddc54972c8607a42ac271325
Ian Keough <ian.keough@autodesk.com>
2015-05-15 07:33:12 -0700

    Stub out test for curve element coloring.

---
+++ test/Libraries/RevitIntegrationTests/DisplayTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Windows.Media;
+using SystemTestServices;
+using Dynamo.UI;
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+
+namespace RevitSystemTests
+{
+    [TestFixture]
+    class DisplayTests : RevitSystemTestBase
+    {
+        [Test, TestModel(@".\empty.rfa")]
+        public void Display_ModelCurveDrawnInBackground_HasCorrectColor()
+        {
+            string samplePath = Path.Combine(workingDirectory, @".\Display\ModelCurveColor.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+            RunCurrentModel();
+
+            //var numLines = BackgroundPreview.Lines.Count;
+            //var expectedColor = (Color)SharedDictionaryManager.DynamoColorsAndBrushesDictionary["EdgeColor"];
+            //var lines = BackgroundPreview.HasNumberOfLinesOfColor();
+        }
+    }
+}
