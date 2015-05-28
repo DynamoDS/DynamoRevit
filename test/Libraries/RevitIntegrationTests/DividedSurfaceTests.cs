c98464c6fb238227c11fd06d043ddec806053729
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-05-28 17:41:38 +0800

    add dividedSurfaceTest in Revit2016

--- test/Libraries/RevitIntegrationTests/DividedSurfaceTests.cs
+++ test/Libraries/RevitIntegrationTests/DividedSurfaceTests.cs
@@ -14,6 +14,8 @@ using RevitTestServices;
 
 using RTF.Framework;
 
+using Revit.Elements;
+
 namespace RevitSystemTests
 {
     [TestFixture]
@@ -27,28 +29,33 @@ namespace RevitSystemTests
 
             string samplePath = Path.Combine(workingDirectory, @".\DividedSurface\DividedSurface.dyn");
             string testPath = Path.GetFullPath(samplePath);
-            
+
             ViewModel.OpenCommand.Execute(testPath);
-            
+
             RunCurrentModel();
             //ViewModel.Model.RunExpression();
 
+            //check DividedSurface
+            var dividedSurfaceID = "729a8380-11f0-4700-80df-14d70accc5ba";
+            var dividedSurface = GetPreviewValueAtIndex(dividedSurfaceID, 0) as Revit.Elements.DividedSurface;
+            Assert.IsNotNull(dividedSurface);
+
             FilteredElementCollector fec = new FilteredElementCollector(DocumentManager.Instance.CurrentUIDocument.Document);
-            fec.OfClass(typeof(DividedSurface));
+            fec.OfClass(typeof(Autodesk.Revit.DB.DividedSurface));
 
             //did it create a divided surface?
             Assert.AreEqual(1, fec.ToElements().Count());
 
-            var ds = (DividedSurface)fec.ToElements()[0];
+            var ds = (Autodesk.Revit.DB.DividedSurface)fec.ToElements()[0];
             Assert.AreEqual(5, ds.USpacingRule.Number);
             Assert.AreEqual(5, ds.VSpacingRule.Number);
 
             //can we change the number of divisions
             var numNode = ViewModel.Model.CurrentWorkspace.Nodes.OfType<DoubleInput>().First();
             numNode.Value = "10";
-           
+
             RunCurrentModel();
-            
+
 
             //did it create a divided surface?
             Assert.AreEqual(10, ds.USpacingRule.Number);
@@ -56,10 +63,12 @@ namespace RevitSystemTests
 
             //ensure there is a warning when we try to set a negative number of divisions
             numNode.Value = "-5";
-           
+
             RunCurrentModel();
-            
+
             Assert.Greater(ViewModel.Model.EngineController.LiveRunnerRuntimeCore.RuntimeStatus.WarningCount, 0);
+
+
         }
     }
 }
