cbf50af005191852e84ccd47ad09d079c92423bd
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-05-29 10:51:51 +0800

    add topographyTest in Revit2016

--- test/Libraries/RevitIntegrationTests/TopographyTests.cs
+++ test/Libraries/RevitIntegrationTests/TopographyTests.cs
@@ -6,6 +6,12 @@ using RevitTestServices;
 
 using RTF.Framework;
 
+using System.Linq;
+
+using Revit.Elements;
+
+using Autodesk.DesignScript.Geometry;
+
 namespace RevitSystemTests
 {
     [TestFixture]
@@ -19,11 +25,18 @@ namespace RevitSystemTests
             string testPath = Path.GetFullPath(samplePath);
 
             ViewModel.OpenCommand.Execute(testPath);
-
-
             RunCurrentModel();
-            
+            AssertNoDummyNodes();
+
+            Assert.AreEqual(10, ViewModel.Model.CurrentWorkspace.Nodes.Count);
+            Assert.AreEqual(13, ViewModel.Model.CurrentWorkspace.Connectors.Count());
+
+            //check Topography.ByPoint
+            var topographyID = "a0b02f6c-a144-4267-b62c-31983661aefa";
+            var topography = GetPreviewValue(topographyID) as Topography;
+            Assert.IsNotNull(topography);
         }
+
 
         [Test]
         [TestModel(@".\Topography\topography.rvt")]
@@ -34,9 +47,20 @@ namespace RevitSystemTests
 
             ViewModel.OpenCommand.Execute(testPath);
 
-
             RunCurrentModel();
-            
+            AssertNoDummyNodes();
+
+            Assert.AreEqual(2, ViewModel.Model.CurrentWorkspace.Nodes.Count);
+            Assert.AreEqual(1, ViewModel.Model.CurrentWorkspace.Connectors.Count());
+
+            //check Topography.Points
+            var pointsID = "c366f888-3b98-4101-9bd7-a1a84e538c61";
+            AssertPreviewCount(pointsID, 1156);
+            for (int i = 0; i < 1156; i++)
+            {
+                var point = GetPreviewValueAtIndex(pointsID, i) as Point;
+                Assert.IsNotNull(point);
+            }
         }
     }
 }
