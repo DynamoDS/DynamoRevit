479e93b6b3670896ada578f24be7f07824bce172
Sancerio <lsancerio@gmail.com>
2015-06-05 14:12:33 +0800

    localize preview value in Revit Nodes

--- src/Libraries/RevitNodes/Elements/Family.cs
+++ src/Libraries/RevitNodes/Elements/Family.cs
@@ -151,7 +151,7 @@ namespace Revit.Elements
 
         public override string ToString()
         {
-            return String.Format("Family: {0}", InternalFamily.Name);
+            return String.Format(Properties.Resources.FamilyPreview, InternalFamily.Name);
         }
 
         #endregion
