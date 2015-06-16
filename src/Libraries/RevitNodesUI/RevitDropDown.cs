ac6dc7eccc0e8305b15b06d827071b0852749b31
Minh Nguyen <minhnb3192@gmail.com>
2015-06-16 09:56:44 +0800

    MAGN-7544

--- src/Libraries/RevitNodesUI/RevitDropDown.cs
+++ src/Libraries/RevitNodesUI/RevitDropDown.cs
@@ -556,7 +556,7 @@ namespace DSRevitNodesUI
     public class StructuralFramingTypes : AllElementsInBuiltInCategory
     {
         public StructuralFramingTypes()
-            : base(BuiltInCategory.OST_StructuralFraming, "Framing Types", "No structural framing types available."){}
+            : base(BuiltInCategory.OST_StructuralFraming, "Framing Types", Properties.Resources.DropDownNoFramingType){}
     }
 
     [NodeName("Structural Column Types")]
@@ -566,7 +566,7 @@ namespace DSRevitNodesUI
     public class StructuralColumnTypes : AllElementsInBuiltInCategory
     {
         public StructuralColumnTypes()
-            : base(BuiltInCategory.OST_StructuralColumns, "Column Types", "No structural column types available."){}
+            : base(BuiltInCategory.OST_StructuralColumns, "Column Types", Properties.Resources.DropDownNoColumnType){}
     }
 
     [NodeName("Spacing Rule Layout")]
