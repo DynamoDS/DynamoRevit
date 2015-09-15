bdfb3fbaff8ce59496f0942e38d36d818ecf14c1
Dimitar Venkov <dimitar.ven@gmail.com>
2015-09-15 15:16:14 +0800

    Create StructuralPlanViewTests.cs

---
+++ test/Libraries/RevitNodesTests/Elements/Views/StructuralPlanViewTests.cs
@@ -0,0 +1,37 @@
+using System;
+
+using NUnit.Framework;
+
+using Revit.Elements;
+using Revit.Elements.Views;
+
+using RevitTestServices;
+
+using RTF.Framework;
+
+namespace RevitNodesTests.Elements.Views
+{
+    [TestFixture]
+    class CeilingPlanViewTests : RevitNodeTestBase
+    {
+        [Test]
+        [TestModel(@".\Empty.rvt")]
+        public void ByLevel_ValidArgs()
+        {
+            var elevation = 100;
+            var level = Level.ByElevation(elevation);
+            Assert.NotNull(level);
+
+            var view = StructuralPlanView.ByLevel(level);
+
+            Assert.NotNull(view);
+        }
+
+        [Test]
+        [TestModel(@".\Empty.rvt")]
+        public void ByLevel_BadArgs()
+        {
+            Assert.Throws(typeof(ArgumentNullException), () => StructuralPlanView.ByLevel(null));
+        }
+    }
+}
