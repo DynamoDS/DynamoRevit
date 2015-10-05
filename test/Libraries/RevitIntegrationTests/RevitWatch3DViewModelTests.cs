8ab8890b7594f0ee039bf073fe2769d29bfa8262
Ian Keough <ian.keough@autodesk.com>
2015-10-05 16:12:38 -0700

    Stub out a test.

---
+++ test/Libraries/RevitIntegrationTests/RevitWatch3DViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using Dynamo.Applications.ViewModel;
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+
+namespace RevitSystemTests
+{
+    [TestFixture]
+    public class RevitWatch3DViewModelTests : RevitSystemTestBase
+    {
+        [Test, TestModel(@".\empty.rfa")]
+        public void SmallCurve_Draw_DoesNotThrowException()
+        {
+            var vm3D = ViewModel.Watch3DViewModels.FirstOrDefault(vm => vm is RevitWatch3DViewModel);
+            Assert.NotNull(vm3D);
+
+            var samplePath = Path.Combine(workingDirectory, @".\RevitWatch3DViewModel\ShortLine.dyn");
+            var testPath = Path.GetFullPath(samplePath);
+
+            Assert.DoesNotThrow(()=>ViewModel.OpenCommand.Execute(testPath));
+        }
+    }
+}
