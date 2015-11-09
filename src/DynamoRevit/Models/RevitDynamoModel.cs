a424465278f2418016ebc99f19cac8a7664dda6d
Michael Kirschner <mjk.kirschner@gmail.com>
2015-11-09 11:16:59 -0500

    reflect changed namespace name

--- src/DynamoRevit/Models/RevitDynamoModel.cs
+++ src/DynamoRevit/Models/RevitDynamoModel.cs
@@ -11,7 +11,7 @@ using DSIronPython;
 using Dynamo.Interfaces;
 using Dynamo.Models;
 using Dynamo.Scheduler;
-using Dynamo.UpdateManager;
+using Dynamo.Updates;
 using Dynamo.Utilities;
 using Greg;
 using Revit.Elements;
