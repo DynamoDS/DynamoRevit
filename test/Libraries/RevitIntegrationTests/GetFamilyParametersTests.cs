c8f69cfc18374b95a34088d58902685b447148bf
Ian Keough <ian.keough@autodesk.com>
2015-11-12 15:54:03 -0800

    Add one test to guard against regression.

---
+++ test/Libraries/RevitIntegrationTests/GetFamilyParametersTests.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using DSRevitNodesUI;
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+
+namespace RevitSystemTests
+{
+    [TestFixture]
+    public class GetFamilyParametersTests : RevitSystemTestBase
+    {
+        [Test, TestModel(@".\Selection\DynamoSample.rvt")]
+        public void CachedValueChanges_HasItemsAndSelectedIndex()
+        {
+            var model = ViewModel.Model;
+            var samplePath = Path.Combine(workingDirectory,
+                                                @".\Family\GetFamilyParameter.dyn");
+            var testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+
+            var parametersNode = model.CurrentWorkspace.Nodes.OfType<FamilyInstanceParameters>().FirstOrDefault();
+            Assert.NotNull(parametersNode);
+
+            Assert.Greater(parametersNode.Items.Count, 0);
+            Assert.AreNotEqual(parametersNode.SelectedIndex, -1);
+        }
+    }
+}
