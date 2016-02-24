9a1dbd663b9842a4b1786261dc90662fb39a275a
Jorgen Dahl <jorgen.dahl@autodesk.com>
2016-02-24 09:11:57 -0500

    MAGN-7419 Added more category by name tests

--- test/Libraries/RevitNodesTests/Elements/CategoryTests.cs
+++ test/Libraries/RevitNodesTests/Elements/CategoryTests.cs
@@ -24,6 +24,24 @@ namespace RevitNodesTests.Elements
 
         [Test]
         [TestModel(@".\empty.rvt")]
+        public void CategoryByName_ValidArgsShort()
+        {
+            var cat = Category.ByName("PointClouds");
+            Assert.NotNull(cat);
+            Assert.AreEqual(cat.Id, cat.InternalCategory.Id.IntegerValue);
+        }
+
+        [Test]
+        [TestModel(@".\empty.rvt")]
+        public void CategoryByName_ValidArgsLong()
+        {
+            var cat = Category.ByName("Point Clouds");
+            Assert.NotNull(cat);
+            Assert.AreEqual(cat.Id, cat.InternalCategory.Id.IntegerValue);
+        }
+
+        [Test]
+        [TestModel(@".\empty.rvt")]
         public void CategoryByName_BadArgs()
         {
             Assert.Throws<ArgumentException>(()=>Category.ByName("foo"));
