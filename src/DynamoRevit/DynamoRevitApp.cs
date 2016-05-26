ce33eebe87b0d1b4ed2a02c95d1241882af4543e
Michael Kirschner <michaelkirschner@Michaels-iMac.local>
2016-05-25 23:18:19 -0400

    before building model subscribe to load event, and gather exceptions, then dump them to preload data.

--- src/DynamoRevit/DynamoRevitApp.cs
+++ src/DynamoRevit/DynamoRevitApp.cs
@@ -309,7 +309,7 @@ namespace Dynamo.Applications
         private void UnsubscribeAssemblyEvents()
         {
             AppDomain.CurrentDomain.AssemblyResolve -= ResolveAssembly;
-            AppDomain.CurrentDomain.AssemblyLoad -= AssemblyLoad;
+            //AppDomain.CurrentDomain.AssemblyLoad -= AssemblyLoad;
         }
 
         /// <summary>
