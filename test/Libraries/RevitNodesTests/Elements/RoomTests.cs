db8a3f7f92052807029a49a87c8d8757910894f0
moethu <maximilian.o.e.thumfart@gmail.com>
2016-06-01 12:06:09 +0100

    Room Element added

---
+++ test/Libraries/RevitNodesTests/Elements/RoomTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+using Autodesk.DesignScript.Geometry;
+
+using Revit.Elements;
+using NUnit.Framework;
+
+using RevitTestServices;
+
+using RTF.Framework;
+
+namespace RevitNodesTests.Elements
+{
+    [TestFixture]
+    public class RoomTests : RevitNodeTestBase
+    {
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rfa")]
+        public void Create_ValidArgs()
+        {
+            Level lvl = Level.ByElevation(0);
+            Assert.IsNotNull(lvl);
+
+            var room = Revit.Elements.Room.Create(lvl,Point.ByCoordinates(0, 0, 0),"myRoom", "myNumber");
+            Assert.NotNull(room);
+            Assert.AreEqual(room.InternalRevitElement.Name, "myRoom");      
+        }
+        
+
+
+
+
+    }
+}
