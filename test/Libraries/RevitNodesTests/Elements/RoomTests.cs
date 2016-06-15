352ab2df705972703bb362fc0c543f7d2292f158
moethu <maximilian.o.e.thumfart@gmail.com>
2016-06-15 10:31:03 +0100

    Added Room Wrapper test

--- test/Libraries/RevitNodesTests/Elements/RoomTests.cs
+++ test/Libraries/RevitNodesTests/Elements/RoomTests.cs
@@ -31,5 +31,30 @@ namespace RevitNodesTests.Elements
             Assert.AreEqual(room.InternalRevitElement.Name, "myRoom");
         }
 
+        /// <summary>
+        /// Test Room Wrapper
+        /// </summary>
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void Create_ValidArgs()
+        {
+            Level lvl = Level.ByElevation(100);
+            Assert.IsNotNull(lvl);
+
+            // Create a new room
+            var room = Revit.Elements.Room.ByLocation(lvl, Point.ByCoordinates(100, 100, 0), "mySecondRoom", "mySecondNumber");
+
+            Assert.NotNull(room);
+
+            // Get the room using the Element Selector
+            Element element = ElementSelector.ByElementId(room.InternalRevitElement.Id.IntegerValue);
+
+            Assert.NotNull(element);
+
+            // Check if the selected element is actually wrapped as a Revit.Element.Room
+            Assert.IsInstanceOf(typeof(Revit.Elements.Room), element);
+        }
+
+
     }
 }
