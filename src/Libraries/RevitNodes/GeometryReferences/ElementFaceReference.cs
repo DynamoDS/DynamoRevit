a76079705dc4c8b4509921bf6e052e185c7370b9
aparajit-pratap <aparajit.pratap@autodesk.com>
2016-07-12 15:41:36 +0800

    Added new Family instance methods to place them ByFace (#979)
    * added FamilyInstance.ByFace methods for MFE project
    
    * family instance byface method
    
    * fixed FamilyInstance.ByFace methods
    
    * attempt to add element binding support for new FamilyInstance.ByFace nodes
    
    * addressed review comments
    
    * added unit test for FamilyInstance.ByFace
    
    * revised test case
    
    * removed unnecessary test case
    
    * added new tests including element binding tests
    
    * updated node descriptions

--- src/Libraries/RevitNodes/GeometryReferences/ElementFaceReference.cs
+++ src/Libraries/RevitNodes/GeometryReferences/ElementFaceReference.cs
@@ -41,13 +41,13 @@ namespace Revit.GeometryReferences
 
         public const string DefaultTag = "RevitFaceReference";
 
-        internal static ElementFaceReference TryGetFaceReference(object curveObject, string nodeTypeString = "This node")
+        internal static ElementFaceReference TryGetFaceReference(object geometryObject, string nodeTypeString = "This node")
         {
-            var curve = (dynamic) curveObject;
+            var geometry = (dynamic) geometryObject;
 
             try
             {
-                return TryGetFaceReference(curve);
+                return TryGetFaceReference(geometry);
             }
             catch (RuntimeBinderException)
             {
@@ -64,23 +64,23 @@ namespace Revit.GeometryReferences
             return curveObject;
         }
 
-        private static ElementFaceReference TryGetFaceReference(Revit.Elements.Element curveObject, string nodeTypeString = "This node")
+        private static ElementFaceReference TryGetFaceReference(Revit.Elements.Element geometryObject, string nodeTypeString = "This node")
         {
-            var cs = curveObject.InternalGeometry().OfType<Autodesk.Revit.DB.Face>();
+            var cs = geometryObject.InternalGeometry().OfType<Autodesk.Revit.DB.Face>();
             if (cs.Any()) return new ElementFaceReference(cs.First());
 
-            var ss = curveObject.InternalGeometry().OfType<Autodesk.Revit.DB.Solid>();
+            var ss = geometryObject.InternalGeometry().OfType<Autodesk.Revit.DB.Solid>();
             if (ss.Any()) return new ElementFaceReference(ss.First().Faces.Cast<Autodesk.Revit.DB.Face>().First());
 
             throw new ArgumentException(string.Format(Properties.Resources.FaceReferenceExtractionFailure, nodeTypeString) +
-                string.Format(Properties.Resources.FaceReferenceExtractionDetail, curveObject));
+                string.Format(Properties.Resources.FaceReferenceExtractionDetail, geometryObject));
         }
 
-        private static ElementFaceReference TryGetFaceReference(Autodesk.DesignScript.Geometry.Surface curveObject, string nodeTypeString = "This node")
+        private static ElementFaceReference TryGetFaceReference(Autodesk.DesignScript.Geometry.Surface surfaceObject, string nodeTypeString = "This node")
         {
             // If a Reference has been added to this object, we can use that
             // to build the Element.
-            object tagObj = curveObject.Tags.LookupTag(DefaultTag);
+            object tagObj = surfaceObject.Tags.LookupTag(DefaultTag);
             if (tagObj != null)
             {
                 var tagRef = (Reference)tagObj;
