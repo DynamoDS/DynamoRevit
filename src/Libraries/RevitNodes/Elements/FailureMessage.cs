58f20a9e763f22c187c8c0f9646006648c2b064f
moethu <maximilian.o.e.thumfart@gmail.com>
2016-07-29 11:20:08 +0100

    PerformanceAdviser added

---
+++ src/Libraries/RevitNodes/Elements/FailureMessage.cs
@@ -0,0 +1,104 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.DesignScript.Runtime;
+using System;
+using RevitServices.Transactions;
+using RevitServices.Persistence;
+using System.Collections.Generic;
+using System.Collections;
+
+
+namespace Revit.Elements
+{
+    /// <summary>
+    /// Performance Adviser Failure Message
+    /// </summary>
+    public class FailureMessage
+    {
+        internal Autodesk.Revit.DB.FailureMessage InternalElement
+        { 
+            get; set;
+        }
+
+        [SupressImportIntoVM]
+        public FailureMessage(Autodesk.Revit.DB.FailureMessage message)
+        {
+            this.InternalElement = message;
+        }
+
+
+        /// <summary>
+        /// The description of the message.
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return InternalElement.GetDescriptionText();
+            }
+        }
+
+        /// <summary>
+        /// The Failing Elements of the message.
+        /// </summary>
+        public ICollection<ElementId> FailingElements
+        {
+            get
+            {
+                return InternalElement.GetFailingElements();
+            }
+        }
+
+
+        /// <summary>
+        /// The additional Failing Elements of the message.
+        /// </summary>
+        public ICollection<ElementId> AdditionalElements
+        {
+            get
+            {
+                return InternalElement.GetAdditionalElements();
+            }
+        }
+
+
+        /// <summary>
+        /// Get the failure severity.
+        /// </summary>
+        public string Severity
+        {
+            get
+            {
+                return InternalElement.GetSeverity().ToString();
+            }
+        }
+     
+        /// <summary>
+        /// Post Messages in Document
+        /// </summary>
+        /// <param name="messages"></param>
+        public static void PostInDocument(IEnumerable<FailureMessage> messages)
+        {
+
+            var document = DocumentManager.Instance.CurrentDBDocument;
+
+            foreach (var msg in messages)
+            {
+                Autodesk.Revit.DB.Transaction tr = new Autodesk.Revit.DB.Transaction(document);
+                tr.Start("Reporting");
+
+                document.PostFailure(msg.InternalElement);
+
+                tr.Commit();
+            }
+
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("{0} : {1}", InternalElement.GetSeverity(), InternalElement.GetDescriptionText());
+        }
+
+        
+    }
+}
