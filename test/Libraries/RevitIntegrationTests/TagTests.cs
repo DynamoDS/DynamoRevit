457ae1f6af5e404044f03617f9732c1fb64feb45
moethu <maximilian.o.e.thumfart@gmail.com>
2016-08-15 15:53:08 +0200

    system tests

---
+++ test/Libraries/RevitIntegrationTests/TagTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Dynamo.Nodes;
+using Autodesk.DesignScript.Geometry;
+using CoreNodeModels.Input;
+using NUnit.Framework;
+using RevitServices.Persistence;
+using RevitTestServices;
+using RTF.Framework;
+
+
+namespace RevitSystemTests
+{
+    [TestFixture]
+    class TagTests : RevitSystemTestBase
+    {
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void PlaceTag()
+        {
+            var model = ViewModel.Model;
+
+            string samplePath = Path.Combine(workingDirectory, @".\Annotations\Tag.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+
+            RunCurrentModel();
+
+            var tagelement = GetPreviewValue("73d7876d-c04a-418c-8f86-2e36c44d9833");
+
+            Assert.IsInstanceOfType(typeof(Revit.Elements.Tag), tagelement.GetType());
+
+        }
+
+
+
+    }
+}
---
+++ test/Libraries/RevitIntegrationTests/TextTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Dynamo.Nodes;
+using Autodesk.DesignScript.Geometry;
+using CoreNodeModels.Input;
+using NUnit.Framework;
+using RevitServices.Persistence;
+using RevitTestServices;
+using RTF.Framework;
+
+
+namespace RevitSystemTests
+{
+    [TestFixture]
+    class TextNoteTests : RevitSystemTestBase
+    {
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void PlaceText()
+        {
+            var model = ViewModel.Model;
+
+            string samplePath = Path.Combine(workingDirectory, @".\Annotations\TextNote.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+
+            RunCurrentModel();
+
+            var textelement = GetPreviewValue("078437f0-b5a8-4690-9506-c3221513f1da");
+
+            Assert.IsInstanceOfType(typeof(Revit.Elements.TextNote), textelement.GetType());
+
+            Revit.Elements.TextNote note = (Revit.Elements.TextNote)textelement;
+
+            Assert.AreEqual(note.Text, "Hello World");
+
+        }
+
+
+
+    }
+}
