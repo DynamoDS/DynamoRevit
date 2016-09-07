17d1eb33f087c67f6e019b36571b8d39122bf8aa
moethu <maximilian.o.e.thumfart@gmail.com>
2016-09-07 10:08:56 +0200

    Annotations added

--- src/Libraries/RevitNodesUI/GenericClasses.cs
+++ src/Libraries/RevitNodesUI/GenericClasses.cs
@@ -171,12 +171,14 @@ namespace DSRevitNodesUI
                 }
             }
 
-            // Parse string to enum value using the Revit.Elements.ParseEnum node
-            var node = AstFactory.BuildFunctionCall("Revit.Elements.ParseEnum", "ByString",
-                 new List<AssociativeNode> { AstFactory.BuildStringNode((string)Items[SelectedIndex].Name), AstFactory.BuildStringNode(this.EnumerationType.FullName) });
+            // get the selected items name
+            var stringNode = AstFactory.BuildStringNode((string)Items[SelectedIndex].Name);
 
-            // Return the selected element
-            return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), node) };
+            // assign the selected name to an actual enumeration value
+            var assign = AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), stringNode);
+
+            // return the enumeration value
+            return new List<AssociativeNode> { assign };
         }
     }
 }
