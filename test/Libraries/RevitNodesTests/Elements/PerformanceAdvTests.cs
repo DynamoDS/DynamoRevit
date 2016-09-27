22ac754953ccd79e3da1dbccf0c65e11ba8df5f6
moethu <maximilian.o.e.thumfart@gmail.com>
2016-09-27 19:38:56 +0200

    testfile

---
+++ test/Libraries/RevitNodesTests/Elements/PerformanceAdvTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+
+using Revit.Elements;
+using NUnit.Framework;
+using Autodesk.Revit.DB;
+using RevitTestServices;
+using System.Collections.Generic;
+using RTF.Framework;
+
+namespace RevitNodesTests.Elements
+{
+    [TestFixture]
+    public class RevisionTests : RevitNodeTestBase
+    {
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void Get_ValidArgs()
+        {
+            PerformanceAdviser adviser = PerformanceAdviser.GetPerformanceAdviser();
+            IList<PerformanceAdviserRuleId> ruleIds = adviser.GetAllRuleIds();
+            var perf = Revit.Elements.PerformanceAdviserRule.ById(ruleIds[0].Guid.ToString());
+            Assert.NotNull(perf);
+
+            Assert.NotNull(perf.Description);
+
+            Assert.NotNull(perf.Name);
+
+            Assert.AreEqual(perf.GetType(), typeof(Revit.Elements.PerformanceAdviserRule));
+        }
+
+
+
+
+    }
+}
