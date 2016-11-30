04404bee5df705f2cf812521235c043c906859a6
moethu <maximilian.o.e.thumfart@gmail.com>
2016-11-30 10:12:17 +0100

    Tag split into two nodes

--- test/Libraries/RevitNodesTests/Elements/TagTests.cs
+++ test/Libraries/RevitNodesTests/Elements/TagTests.cs
@@ -30,7 +30,48 @@ namespace RevitNodesTests.Elements
 
             var tag = Tag.ByElement(Revit.Application.Document.Current.ActiveView, wall, true, false, "Center", "Middle", Vector.ByCoordinates(0, 0, 0));
             Assert.NotNull(tag);
+
+            tag = Tag.ByElementAndLocation(Revit.Application.Document.Current.ActiveView, wall, Point.ByCoordinates(0,0,0));
+            Assert.NotNull(tag);
+            tag.GetLocation().DistanceTo(Point.ByCoordinates(0, 0, 0)).ShouldBeApproximately(0);
+
+            tag = Tag.ByElementAndOffset(Revit.Application.Document.Current.ActiveView, wall, Vector.ByCoordinates(0,0,0));
+            Assert.NotNull(tag);
           
+        }
+
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void ByElementAndLocation_ValidArgs()
+        {
+            var line = Line.ByStartPointEndPoint(Point.ByCoordinates(0, 0, 0), Point.ByCoordinates(1000, 0, 0));
+            Assert.NotNull(line);
+
+            Autodesk.Revit.DB.FilteredElementCollector collector = new Autodesk.Revit.DB.FilteredElementCollector(Revit.Application.Document.Current.InternalDocument).OfClass(typeof(Autodesk.Revit.DB.WallType));
+            Revit.Elements.WallType wt = Revit.Elements.WallType.FromExisting((Autodesk.Revit.DB.WallType)collector.FirstOrDefault(), true);
+
+            var wall = Wall.ByCurveAndHeight(line, 300, Level.ByElevation(100), wt);
+            Assert.NotNull(wall);
+
+            var tag = Tag.ByElementAndLocation(Revit.Application.Document.Current.ActiveView, wall, Point.ByCoordinates(0, 0, 0));
+            Assert.NotNull(tag);
+        }
+
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void ByElementAndOffset_ValidArgs()
+        {
+            var line = Line.ByStartPointEndPoint(Point.ByCoordinates(0, 0, 0), Point.ByCoordinates(1000, 0, 0));
+            Assert.NotNull(line);
+
+            Autodesk.Revit.DB.FilteredElementCollector collector = new Autodesk.Revit.DB.FilteredElementCollector(Revit.Application.Document.Current.InternalDocument).OfClass(typeof(Autodesk.Revit.DB.WallType));
+            Revit.Elements.WallType wt = Revit.Elements.WallType.FromExisting((Autodesk.Revit.DB.WallType)collector.FirstOrDefault(), true);
+
+            var wall = Wall.ByCurveAndHeight(line, 300, Level.ByElevation(100), wt);
+            Assert.NotNull(wall);
+
+            var tag = Tag.ByElementAndOffset(Revit.Application.Document.Current.ActiveView, wall, Vector.ByCoordinates(0, 0, 0));
+            Assert.NotNull(tag);
         }
 
 
