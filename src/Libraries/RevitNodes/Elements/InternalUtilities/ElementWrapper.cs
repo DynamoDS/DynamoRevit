28fe0f1e92394d81cf2e3fcd89f942b1d8dabcce
Konrad K Sobon <ksobon1986@gmail.com>
2017-01-28 17:04:59 -0500

    import instance wrapper

--- src/Libraries/RevitNodes/Elements/InternalUtilities/ElementWrapper.cs
+++ src/Libraries/RevitNodes/Elements/InternalUtilities/ElementWrapper.cs
@@ -283,6 +283,11 @@ namespace Revit.Elements
 
         }
 
+        public static ImportInstance Wrap(Autodesk.Revit.DB.ImportInstance ele, bool isRevitOwned)
+        {
+            return ImportInstance.FromExisting(ele, isRevitOwned);
+        }
+
 
         #endregion
 
