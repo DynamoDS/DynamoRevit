1a783a459e11f25f755b17b62fecdeae7ea9e910
QilongTang <173288704@qq.com>
2017-01-31 02:34:23 -0500

    CherryPickPerformanceAdviser

---
+++ test/Libraries/RevitNodesTests/Elements/PerformanceAdvTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+
+namespace RevitNodesTests.Elements
+{
+    [TestFixture]
+    public class PerformanceAdviserTests : RevitNodeTestBase
+    {
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void Get_ValidArgs()
+        {
+            PerformanceAdviser adviser = PerformanceAdviser.GetPerformanceAdviser();
+            IList<PerformanceAdviserRuleId> ruleIds = adviser.GetAllRuleIds();
+            var perf = Revit.Elements.PerformanceAdviserRule.ById(ruleIds[0].Guid.ToString());
+            Assert.NotNull(perf);
+
+            Assert.NotNull(perf.Description);
+
+            Assert.NotNull(perf.Name);
+
+            Assert.AreEqual(perf.GetType(), typeof(Revit.Elements.PerformanceAdviserRule));
+        }
+
+        [Test]
+        [TestModel(@".\emptyAnnotativeView.rvt")]
+        public void Execute_ValidResult()
+        {
+            PerformanceAdviser adviser = PerformanceAdviser.GetPerformanceAdviser();
+            IList<PerformanceAdviserRuleId> ruleIds = adviser.GetAllRuleIds();
+            var perf = Revit.Elements.PerformanceAdviserRule.ById(ruleIds[0].Guid.ToString());
+            List<Revit.Elements.PerformanceAdviserRule> rules = new List<Revit.Elements.PerformanceAdviserRule>(){perf};
+
+            var messages = Revit.Elements.PerformanceAdviserRule.Execute(rules);
+
+            foreach (var msg in messages)
+            {
+                Assert.IsTrue(msg.GetType() == typeof(Revit.Elements.FailureMessage));
+                Assert.IsNotNull(msg.Description);
+                Assert.IsNotNull(msg.Severity);
+            }
+        }
+
+
+    }
+}
