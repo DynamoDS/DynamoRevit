dfb01b54923659af8edcac7f9255feb70f14fb08
Aaron (Qilong) <aaron.tang@autodesk.com>
2018-04-06 13:40:16 -0400

    CherryPick (#1984)

--- src/restorepackages.bat
+++ src/restorepackages.bat
@@ -1,4 +1,4 @@
-@echo off
+﻿@echo off
 REM ***********************************************************************
 REM This bat file uses Aget: https://git.autodesk.com/Dynamo/Aget to 
 REM download the nuget packages from NuGet server or closest Artifactory server for 
@@ -24,23 +24,7 @@ set NugetExe=%CurrentDir%\Tools\NugetCLI\nuget.exe
 set AgetFile=%CurrentDir%\Tools\Aget\aget.exe
 set NugetConfig=%ConfigDir%\dynamo-nuget.config
 
-REM 2. replace "LatestBeta" strings in packages-template.aget with the latest
-REM    pre-release version of DynamoVisualProgramming.Core package,
-REM    and save the replaced file as packages.aget
-set versionQueryParams=list DynamoVisualProgramming.Core -prerelease -config "%NugetConfig%"
-for /F "tokens=2 delims= " %%F in ( 'call "%NugetExe%" %versionQueryParams%' ) do (set LatestBeta=%%F)
-echo Latest pre-release version of "DynamoVisualProgramming.Core" package is "%LatestBeta%"
-
-if exist "%ConfigDir%\packages.aget" del "%ConfigDir%\packages.aget"
-for /f "tokens=* delims=¶" %%i in ( 'type "%ConfigDir%\packages-template.aget"' ) do (
-    set line=%%i
-    setlocal EnableDelayedExpansion
-    set line=!line:LatestBeta=%LatestBeta%!
-    echo !line!>>"%ConfigDir%\packages.aget"
-    endlocal
-)
-
-REM 3. download 3rdParty packages by Aget.exe
+REM 2. download 3rdParty packages by Aget.exe
     echo Running Python script from %AgetFile% using dynamo-nuget.config file
     set PythonAget="%AgetFile%" -os win -config release -iset intel64 -toolchain v140 -linkage shared -packagesDir "%DynamoPackages%" -nuget "%NugetExe%" -framework net45 -nugetConfig "%NugetConfig%"
 
