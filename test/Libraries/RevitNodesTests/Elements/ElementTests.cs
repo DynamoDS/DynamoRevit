8b938add67f4853cbb85a29681c041fdf787d2dc
Sylvester Knudsen <sylvesterknudsen@gmail.com>
2019-11-25 12:49:13 +0000

    Element.unjoin geometry (#7)
    * Element.is joined (#4)
    
    * IsJoined method
    * unit test for IsJoined
    * system test for IsJoined
    * system test sample file
    * Update src/Libraries/RevitNodes/Elements/Element.cs
    Co-Authored-By: Radu Gidei <radumg@users.noreply.github.com>
    * Update test/Libraries/RevitIntegrationTests/ElementTests.cs
    Co-Authored-By: Radu Gidei <radumg@users.noreply.github.com>
    * naming updates
    * update integration test
    * update unit test
    * Delete IsJoined_System_Test.dyn
    
    * changed test file names
    
    * Element.IsPinned (#1)
    
    Element.IsPinned node + unit and system tests.
    Adds a node to the Elements.Element category that returns the pinned status of that element
    
    * Element.SetPinnedStatus (#2)
    
    Element.SetPinnedStatus + unit and system tests
    Adds a node for pinning or unpinning elements
    
    * Element.HostedElements (#3)
    
    Element.HostedElements node + unit and system tests
    Adds node to Elments.Element that gets all elements hosted to the input element
    
    * Element.get joined elements (#5)
    
    Element.GetJoinedElements node + unit and system tests
    Adds node to Elements.Element that gets all elements joined with the input element
    
    * test updates
    
    * UnjoinGeometry node
    
    * Element.UnjoinGeometry tests

--- test/Libraries/RevitNodesTests/Elements/ElementTests.cs
+++ test/Libraries/RevitNodesTests/Elements/ElementTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 using Autodesk.DesignScript.Geometry;
 using Autodesk.Revit.DB;
@@ -260,6 +261,212 @@ namespace RevitNodesTests.Elements
 
         #endregion
 
+        #region Pin settings
 
+        /// <summary>
+        /// gets the pinned status of an element from the model
+        /// and checks if IsPinned is the correct value
+        /// </summary>
+        [Test]
+        [TestModel(@".\Element\elementPinned.rvt")]
+        public void CanSuccessfullyGetElementPinnedStatus()
+        {
+            var pinnedElement = ElementSelector.ByElementId(184176, true);
+            AssertElementPinnedStatusIs(pinnedElement, true);
+
+            var unPinnedElement = ElementSelector.ByElementId(184324, true);
+            AssertElementPinnedStatusIs(unPinnedElement, false);
+        }
+
+        /// <summary>
+        /// Checks if Pin status can be set correctly
+        /// </summary>
+        [Test]
+        [TestModel(@".\element.rvt")]
+        public void CanSuccessfullySetElementPinnedStatus()
+        {
+            var elem = ElementSelector.ByElementId(184176, true);
+            Assert.IsNotNull(elem);
+
+            bool originalPinStatus = elem.IsPinned;
+
+            elem.SetPinnedStatus(true);
+            Assert.AreNotEqual(originalPinStatus, elem.IsPinned);
+
+            elem.SetPinnedStatus(false);
+            Assert.AreEqual(originalPinStatus, elem.IsPinned);
+        }
+
+        private static void AssertElementPinnedStatusIs(
+            Element element,
+            bool expectedValue)
+        {
+            bool pinStatus = element.IsPinned;
+
+            Assert.NotNull(pinStatus);
+            Assert.AreEqual(expectedValue, pinStatus);
+        }
+        #endregion
+        #region Join tests
+
+        [Test]
+        [TestModel(@".\Element\elementJoin.rvt")]
+        public void CanSuccessfullyGetJoinedElementsFromElement()
+        {
+            // Arrange - get element from model
+            var element = ElementSelector.ByElementId(184176, true);
+            int[] expectedIds = new int[] { 207960, 208259, 208422 };
+
+            // Act
+            Element[] joinedElements = element.GetJoinedElements();
+            var joinedElementIds = joinedElements
+                .Select(x => x.Id)
+                .ToArray();
+
+            // Assert
+            CollectionAssert.AreEqual(expectedIds, joinedElementIds);
+        }
+        
+        [TestModel(@".\Element\hostedElements.rvt")]
+        public void CanSuccessfullyGetHostedElements()
+        {
+            // Arrange - Select the wall element in revit by it Id
+            var elem = ElementSelector.ByElementId(261723, true);
+            // Model element names
+            string windowFamName = "600 x 3100";
+            string curtainWallFamName = "Curtain Wall";
+            string wallOpeningFamName = "Rectangular Straight Wall Opening";
+            // Expected hosted elements lists
+            var famNamesWithShadowsInserts = new[] { windowFamName, windowFamName, windowFamName };
+            var famNamesWithEverything = new[] { curtainWallFamName, windowFamName, windowFamName, windowFamName, wallOpeningFamName };
+            var famNamesWithOpeningsShadows = new[] { windowFamName, windowFamName, windowFamName, wallOpeningFamName };
+            var famNamesWithShadowEmbeddedWallsInserts = new[] { curtainWallFamName, windowFamName, windowFamName, windowFamName };
+
+            // Act - Invoke GetHostedElements with all possible cobinations
+            var hostedElementsIncludeNothing = elem.GetHostedElements();
+            var hostedElementsIncludeEverything = elem.GetHostedElements(true, true, true, true);
+
+            var hostedElementsIncludeOpenings = elem.GetHostedElements(true, false, false, false);
+            var hostedElementsIncludeOpeningsAndShadows = elem.GetHostedElements(true, true, false, false);
+            var hostedElementsIncludeOpeningsAndShadowsAndEmbeddedWalls = elem.GetHostedElements(true, true, true, false);
+
+            var hostedElementsIncludeShadows = elem.GetHostedElements(false, true, false, false);
+            var hostedElementsIncludeShadowsAndEmbeddedWalls = elem.GetHostedElements(false, true, true, false);
+            var hostedElementsIncludeShadowsAndEmbeddedWallsAndEmbeddedInserts = elem.GetHostedElements(false, true, true, true);
+
+            var hostedElementsIncludeEmbeddedWalls = elem.GetHostedElements(false, false, true, false);
+            var hostedElementsIncludeEmbeddedWallsAndEmbeddedInserts = elem.GetHostedElements(false, false, true, true);
+            var hostedElementsIncludeEmbeddedWallsAndEmbeddedInsertsAndOpenings = elem.GetHostedElements(true, false, true, true);
+
+            var hostedElementsIncludeEmbeddedInserts = elem.GetHostedElements(false, false, false, true);
+            var hostedElementsIncludeSEmbeddedInsertsAndOpenings = elem.GetHostedElements(true, false, false, true);
+            var hostedElementsIncludeEmbeddedInsertsAndOpeningsAndShadows = elem.GetHostedElements(true, true, false, true);
+
+            //Assert all combinations has the right amount of output elements
+            Assert.AreEqual(3, hostedElementsIncludeNothing.Count);
+            Assert.AreEqual(5, hostedElementsIncludeEverything.Count);
+
+            Assert.AreEqual(4, hostedElementsIncludeOpenings.Count);
+            Assert.AreEqual(4, hostedElementsIncludeOpeningsAndShadows.Count);
+            Assert.AreEqual(5, hostedElementsIncludeOpeningsAndShadowsAndEmbeddedWalls.Count);
+
+            Assert.AreEqual(3, hostedElementsIncludeShadows.Count);
+            Assert.AreEqual(4, hostedElementsIncludeShadowsAndEmbeddedWalls.Count);
+            Assert.AreEqual(4, hostedElementsIncludeShadowsAndEmbeddedWallsAndEmbeddedInserts.Count);
+
+            Assert.AreEqual(4, hostedElementsIncludeEmbeddedWalls.Count);
+            Assert.AreEqual(4, hostedElementsIncludeEmbeddedWallsAndEmbeddedInserts.Count);
+            Assert.AreEqual(5, hostedElementsIncludeEmbeddedWallsAndEmbeddedInsertsAndOpenings.Count);
+
+            Assert.AreEqual(3, hostedElementsIncludeEmbeddedInserts.Count);
+            Assert.AreEqual(4, hostedElementsIncludeSEmbeddedInsertsAndOpenings.Count);
+            Assert.AreEqual(4, hostedElementsIncludeEmbeddedInsertsAndOpeningsAndShadows.Count);
+
+            //Assert all combinations has the right elements as output
+            CollectionAssert.AreEqual(famNamesWithShadowsInserts, hostedElementsIncludeNothing.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithEverything, hostedElementsIncludeEverything.Select(x => x.Name).ToArray());
+
+            CollectionAssert.AreEqual(famNamesWithOpeningsShadows, hostedElementsIncludeOpenings.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithOpeningsShadows, hostedElementsIncludeOpeningsAndShadows.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithEverything, hostedElementsIncludeOpeningsAndShadowsAndEmbeddedWalls.Select(x => x.Name).ToArray());
+
+            CollectionAssert.AreEqual(famNamesWithShadowsInserts, hostedElementsIncludeShadows.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithShadowEmbeddedWallsInserts, hostedElementsIncludeShadowsAndEmbeddedWalls.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithShadowEmbeddedWallsInserts, hostedElementsIncludeShadowsAndEmbeddedWallsAndEmbeddedInserts.Select(x => x.Name).ToArray());
+
+            CollectionAssert.AreEqual(famNamesWithShadowEmbeddedWallsInserts, hostedElementsIncludeEmbeddedWalls.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithShadowEmbeddedWallsInserts, hostedElementsIncludeEmbeddedWallsAndEmbeddedInserts.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithEverything, hostedElementsIncludeEmbeddedWallsAndEmbeddedInsertsAndOpenings.Select(x => x.Name).ToArray());
+
+            CollectionAssert.AreEqual(famNamesWithShadowsInserts, hostedElementsIncludeEmbeddedInserts.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithOpeningsShadows, hostedElementsIncludeSEmbeddedInsertsAndOpenings.Select(x => x.Name).ToArray());
+            CollectionAssert.AreEqual(famNamesWithOpeningsShadows, hostedElementsIncludeEmbeddedInsertsAndOpeningsAndShadows.Select(x => x.Name).ToArray());
+        }
+
+        [Test]
+        [TestModel(@".\Element\elementJoin.rvt")]
+        public void CanSuccessfullyCheckIfTwoElementsAreJoined()
+        {
+            var wall1 = ElementSelector.ByElementId(184176, true);
+            var wall2 = ElementSelector.ByElementId(207960, true);
+            var floor = ElementSelector.ByElementId(208259, true);
+            var beam1 = ElementSelector.ByElementId(208422, true);
+            var beam2 = ElementSelector.ByElementId(208572, true);
+
+            // Check if different kinds of elements are joined
+            // wall1 and wall2 are joined
+            AssertElementsAreJoined(wall1, wall2, true);
+            // wall1 and floor are joined
+            AssertElementsAreJoined(wall1, floor, true);
+            // wall2 and floor are not joined
+            AssertElementsAreJoined(wall2, floor, false);
+            // wall1 and beam1 are joined
+            AssertElementsAreJoined(wall1, beam1, true);
+            // wall2 and beam2 are not joined
+            AssertElementsAreJoined(wall2, beam2, false);
+            // beam1 and beam2 are joined
+            AssertElementsAreJoined(beam1, beam2, true);
+            // beam1 and floor are joined
+            AssertElementsAreJoined(beam1, floor, true);
+        }
+        private static void AssertElementsAreJoined(Element element, Element otherElement, bool expected)
+        {
+            bool arejoined = element.AreJoined(otherElement);
+            Assert.AreEqual(expected, arejoined);
+        }
+
+        [Test]
+        [TestModel(@".\Element\elementJoin.rvt")]
+        public void CanSuccessfullyUnjoinListOfElements()
+        {
+            var wall1 = ElementSelector.ByElementId(184176, true);
+            var wall2 = ElementSelector.ByElementId(207960, true);
+            var floor = ElementSelector.ByElementId(208259, true);
+
+            // Are joined
+            bool orignalWall1AndWall2JoinedValue = wall1.AreJoined(wall2);
+            // Are joined
+            bool orignalWall1AndFloorJoinedValue = wall1.AreJoined(floor);
+            // Are not joined
+            bool orignalWall2AndFloorJoinedValue = wall2.AreJoined(floor);
+
+            var elementList = new List<Element>() { wall1, wall2, floor };
+
+            Element.UnjoinGeometry(elementList);
+
+            bool newWall1AndWall2JoinedValue = wall1.AreJoined(wall2);
+            bool newWall1AndFloorJoinedValue = wall1.AreJoined(floor);
+            bool newWall2AndFloorJoinedValue = wall2.AreJoined(floor);
+
+            // Are joined should have changed
+            Assert.AreNotEqual(newWall1AndWall2JoinedValue, orignalWall1AndWall2JoinedValue);
+            // Are joined should have changed 
+            Assert.AreNotEqual(newWall1AndFloorJoinedValue, orignalWall1AndFloorJoinedValue);
+            // Are joined should be the same 
+            Assert.AreEqual(newWall2AndFloorJoinedValue, orignalWall2AndFloorJoinedValue);
+
+        }
+
+        #endregion
     }
 }
