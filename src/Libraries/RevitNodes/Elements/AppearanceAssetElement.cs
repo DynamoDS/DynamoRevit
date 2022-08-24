11eb1a04be35eab45850a1f6ddf05196c0b6f418
WangyangShi <62596335+wangyangshi@users.noreply.github.com>
2022-08-23 19:50:45 -0700

    Shiw/cherry pick param bug fix and material nodes (#2856)
    * Add new material node and appearance asset element (#2807)
    
    * Add a Materail Node and a new class AppearanceAssetElement.
    
    * Add Systemtests for new nodes.
    
    * Make dynamo node for material texture be easier for end user (#2836)
    
    Co-authored-by: Joe-Yanchun Qiao <yanchun.qiao@autodesk.com>
    
    * Make exception mesage more friendly. (#2852)
    
    Co-authored-by: Joe-Yanchun Qiao <yanchun.qiao@autodesk.com>
    
    * Fix the bug that cannot set unmeasurable parameter value in Dynamo. (#2846)
    
    Co-authored-by: Honghui Qin <106515738+honghuiqin@users.noreply.github.com>
    
    * bump d4r version in 2023.1
    
    Co-authored-by: ZiyunShang <33445445+ZiyunShang@users.noreply.github.com>
    Co-authored-by: joethales <nanqiaomu@hotmail.com>
    Co-authored-by: Joe-Yanchun Qiao <yanchun.qiao@autodesk.com>
    Co-authored-by: Honghui Qin <106515738+honghuiqin@users.noreply.github.com>

---
+++ src/Libraries/RevitNodes/Elements/AppearanceAssetElement.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamoServices;
+using RevitServices.Transactions;
+using Autodesk.DesignScript.Runtime;
+using System.Reflection;
+
+namespace Revit.Elements
+{
+    [RegisterForTrace]
+    public class AppearanceAssetElement : Element
+    {
+        #region Internal properties
+
+        /// <summary>
+        /// Internal reference to the Element
+        /// </summary>
+        internal Autodesk.Revit.DB.AppearanceAssetElement InternalAppearanceAssetElement
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Reference to the Element
+        /// </summary>
+        public override Autodesk.Revit.DB.Element InternalElement
+        {
+            get { return InternalAppearanceAssetElement; }
+        }
+
+        #endregion
+
+        #region Private constructors
+
+        /// <summary>
+        /// Private constructor for DSAppearanceAssetElement
+        /// </summary>
+        /// <param name="appearanceAssetElement"></param>
+        private AppearanceAssetElement(Autodesk.Revit.DB.AppearanceAssetElement appearanceAssetElement)
+        {
+            SafeInit(() => InitAppearanceAssetElement(appearanceAssetElement), true);
+        }
+
+        #endregion
+
+        #region Helper for private constructors
+
+        /// <summary>
+        /// Initialize a AppearanceAssetElement element
+        /// </summary>
+        /// <param name="appearanceAssetElement"></param>
+        private void InitAppearanceAssetElement(Autodesk.Revit.DB.AppearanceAssetElement appearanceAssetElement)
+        {
+            InternalSetAppearanceAssetElement(appearanceAssetElement);
+        }
+
+        #endregion
+
+        #region Private mutators
+
+        /// <summary>
+        /// Set the internal Element, ELementId, and UniqueId
+        /// </summary>
+        /// <param name="appearanceAssetElement"></param>
+        private void InternalSetAppearanceAssetElement(Autodesk.Revit.DB.AppearanceAssetElement appearanceAssetElement)
+        {
+            this.InternalAppearanceAssetElement = appearanceAssetElement;
+            this.InternalElementId = appearanceAssetElement.Id;
+            this.InternalUniqueId = appearanceAssetElement.UniqueId;
+        }
+
+      #endregion
+
+      #region public methods
+
+      /// <summary>
+      /// Get texture image path of the appearance asset
+      /// </summary>
+      /// <returns name = "imageProperties">The list of all the properties that have images connected. A property's meaning can be queried in Revit API documentation, e.g., for "GenericDiffuse.UnifiedbitmapBitmap", it means property "GenericDiffuse" connects a texture asset which has a "UnifiedbitmapBitmap" property</returns>
+      /// <returns name = "imagePaths">The list of all the image path</returns>
+      [MultiReturn(new[] { "imageProperties", "imagePaths" })]
+        public Dictionary<string, object> GetRenderingAssetTextureImages()
+        {
+            var asset = this.InternalAppearanceAssetElement.GetRenderingAsset();
+            List<string> keys = new List<string>();
+            List<string> values = new List<string>();
+            List<string> KeyValue = new List<string>();
+            KeyValue.AddRange(GetAssetProperties(asset, ""));
+            foreach(var value in KeyValue)
+            {
+               var strs = value.Split(new Char[] { ':' }, 2);
+               if (strs.Length == 2)
+               {
+                  keys.Add(strs[0]);
+                  values.Add(strs[1]); 
+               }
+            }
+            Dictionary<string, object> keyValues = new Dictionary<string, object>();
+            keyValues.Add("imageProperties", keys);
+            keyValues.Add("imagePaths", values);
+            return keyValues;
+        }
+
+      /// <summary>
+      /// Set texture image path of the appearance asset
+      /// </summary>
+      /// <param name="imageProperty">A property that has a image connected. This can be queried by calling GetRenderingAssetTextureImages first</param>
+      /// <param name="imagePath">File path of the image</param>
+      /// <returns>AppearanceAssetElement</returns>
+      public AppearanceAssetElement SetRenderingAssetTextureImage(string imageProperty, string imagePath)
+        {
+            var properties = imageProperty.Split('.');
+            TransactionManager.Instance.EnsureInTransaction(Application.Document.Current.InternalDocument);
+            {
+                using (Autodesk.Revit.DB.Visual.AppearanceAssetEditScope editScope =
+                    new Autodesk.Revit.DB.Visual.AppearanceAssetEditScope(Application.Document.Current.InternalDocument))
+                {
+                    Autodesk.Revit.DB.Visual.Asset editableAsset = editScope.Start(InternalElementId);
+                    Type assetClass = GetAssetClass(editableAsset);
+                    PropertyInfo pinfo = assetClass.GetProperty(properties[0]);
+                    if (pinfo == null)
+                    {
+                        throw new ArgumentException(string.Format(Properties.Resources.AppearanceAssetElementPropertyPathInvalid, imageProperty, properties[0], assetClass.Name));
+                    }
+                    object internalProp = pinfo.GetValue(null);
+                    Autodesk.Revit.DB.Visual.AssetProperty assetProperty = editableAsset.FindByName(internalProp.ToString());
+                    Autodesk.Revit.DB.Visual.Asset connentedAsset = assetProperty.GetSingleConnectedAsset();
+                    for (int i = 1; i < properties.Length; i++)
+                    {
+                        if(connentedAsset == null)
+                        {
+                            throw new ArgumentException(string.Format(Properties.Resources.AppearanceAssetElementPropertyPathNoConnect, imageProperty, properties[0], assetClass.Name));
+                        }
+                        Type connentedAssetClass = GetAssetClass(connentedAsset);
+                        PropertyInfo connentedPinfo = connentedAssetClass.GetProperty(properties[i]);
+                        if (connentedPinfo == null)
+                        {
+                           throw new ArgumentException(string.Format(Properties.Resources.AppearanceAssetElementPropertyPathInvalid, imageProperty, properties[i], connentedAssetClass.Name));
+                        }
+                        object connentedInternalProp = connentedPinfo.GetValue(null);
+                        assetProperty = connentedAsset.FindByName(connentedInternalProp.ToString());
+                        connentedAsset = assetProperty.GetSingleConnectedAsset();
+                    }
+                    if (assetProperty != null)
+                    {
+                        if (assetProperty.Name == Autodesk.Revit.DB.Visual.UnifiedBitmap.UnifiedbitmapBitmap)
+                        {
+                           (assetProperty as Autodesk.Revit.DB.Visual.AssetPropertyString).Value = imagePath;
+                        }
+                        else
+                        {
+                           throw new ArgumentException(string.Format(Properties.Resources.AppearanceAssetElementPropertyPathNoConnect, imageProperty, properties.Last(), "UnifiedBitmap"));
+                        }
+
+                    }
+                    else
+                    {
+                        throw new ArgumentException(string.Format(Properties.Resources.AppearanceAssetElementPropertyPathInvalid, imageProperty, properties.Last(), "UnifiedBitmap"));
+                    }
+                    editScope.Commit(true);
+                }
+            }
+            TransactionManager.Instance.TransactionTaskDone();
+            return this;
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        private List<string> GetAssetProperties(Autodesk.Revit.DB.Visual.Asset asset, string keyName)
+        {
+            Type assetClass = GetAssetClass(asset);
+            Dictionary<string, string> InternalPropertiesMap = new Dictionary<string, string>();
+            foreach (var p in assetClass.GetProperties())
+            {
+               var v = p.GetValue(null);
+               InternalPropertiesMap.Add(v.ToString(), p.Name);
+            }
+            List<string> AssetProps = new List<string>();
+            Autodesk.Revit.DB.Visual.AssetProperty nameProperty = asset.FindByName("BaseSchema");
+            string assetSchema = (nameProperty as Autodesk.Revit.DB.Visual.AssetPropertyString).Value;
+            if (assetSchema == "UnifiedBitmapSchema")
+            {
+                var path = asset.FindByName(Autodesk.Revit.DB.Visual.UnifiedBitmap.UnifiedbitmapBitmap) as Autodesk.Revit.DB.Visual.AssetPropertyString;
+                if (path != null) 
+                {
+                    string APIProperty;
+                    if (InternalPropertiesMap.TryGetValue(path.Name, out APIProperty))
+                    {
+                        string newKeyName = keyName + "." + APIProperty;
+                        AssetProps.Add(newKeyName + ":" + path.Value);
+                        return AssetProps;
+                    }
+                }
+            }
+
+            for (int i = 0; i < asset.Size; i++)
+            {
+                Autodesk.Revit.DB.Visual.AssetProperty assetProperty = asset.Get(i);
+                if (assetProperty.NumberOfConnectedProperties > 0)
+                {
+                    var connProps = assetProperty.GetAllConnectedProperties();
+                    foreach(var prop in connProps)
+                    {
+                        string APIProperty;
+                        if (InternalPropertiesMap.TryGetValue(assetProperty.Name, out APIProperty))
+                        {
+                           string newKeyName = String.IsNullOrEmpty(keyName) ? APIProperty : keyName + "." + APIProperty;
+                           AssetProps.AddRange(GetAssetProperties(prop as Autodesk.Revit.DB.Visual.Asset, newKeyName));
+                        }
+                    }
+                }
+            }
+
+            return AssetProps;
+        }
+
+        private Type GetAssetClass(Autodesk.Revit.DB.Visual.Asset asset)
+        {
+            Autodesk.Revit.DB.Visual.AssetProperty nameProperty = asset.FindByName("BaseSchema");
+            string assetSchema = (nameProperty as Autodesk.Revit.DB.Visual.AssetPropertyString).Value;
+            string assetType = "Autodesk.Revit.DB.Visual." + assetSchema.Substring(0, assetSchema.Length - 6);
+            // Get Revit Assembly
+            var revitAssembly = System.Reflection.Assembly.GetAssembly(typeof(Autodesk.Revit.DB.Visual.Asset));
+            // Get Type from Assembly by string
+            Type assetClass = revitAssembly.GetType(assetType);
+            return assetClass;
+        }
+        #endregion
+
+        #region Internal static constructors
+
+        /// <summary>
+        /// Wrap an element in the associated DS type
+        /// </summary>
+        /// <param name="appearanceAssetElement">The appearanceAssetElement</param>
+        /// <param name="isRevitOwned"></param>
+        /// <returns></returns>
+        internal static AppearanceAssetElement FromExisting(Autodesk.Revit.DB.AppearanceAssetElement appearanceAssetElement, bool isRevitOwned)
+        {
+            return new AppearanceAssetElement(appearanceAssetElement)
+            {
+                IsRevitOwned = isRevitOwned
+            };
+        }
+
+        #endregion
+    }
+}
--- src/Libraries/RevitNodes/Properties/Resources.Designer.cs
+++ src/Libraries/RevitNodes/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Revit.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -84,6 +84,24 @@ namespace Revit.Properties {
         internal static string AnalysisResultsDefaultName {
             get {
                 return ResourceManager.GetString("AnalysisResultsDefaultName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Input value for imageProperty {0} is invalid. There is no {1} in {2}..
+        /// </summary>
+        internal static string AppearanceAssetElementPropertyPathInvalid {
+            get {
+                return ResourceManager.GetString("AppearanceAssetElementPropertyPathInvalid", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Input value for imageProperty {0} is invalid. There is no texture connected to {1} in {2}..
+        /// </summary>
+        internal static string AppearanceAssetElementPropertyPathNoConnect {
+            get {
+                return ResourceManager.GetString("AppearanceAssetElementPropertyPathNoConnect", resourceCulture);
             }
         }
         
