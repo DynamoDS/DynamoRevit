765d7300d31d7af4b0e12ac2ebae4d2ea71f29fb
Deyan Nenov <dnenov@archilizer.com>
2023-06-06 07:34:58 +0100

    Recreated 3dview-wrapper pointing to master branch (#2925)

---
+++ src/Libraries/RevitNodes/Elements/Views/View3DTemplate.cs
@@ -0,0 +1,60 @@
+﻿using DynamoServices;
+using System;
+
+namespace Revit.Elements.Views
+{
+    /// <summary>
+    /// A Revit 3D View Template
+    /// </summary>
+    [RegisterForTrace]
+    public class View3DTemplate : View3D
+    {
+
+        #region Private constructors
+
+        /// <summary>
+        /// Create a Revit Floor Plan from Autodesk View Plan
+        /// </summary>
+        private View3DTemplate(Autodesk.Revit.DB.View3D view)
+        {
+            SafeInit(() => Init3DView(view), true);
+        }
+
+        #endregion
+
+        #region Helpers for private constructors
+
+        /// <summary>
+        /// Initialize a FloorPlanView element
+        /// </summary>
+        private void Init3DView(Autodesk.Revit.DB.View3D view)
+        {
+            InternalSetView3D(view);
+        }
+
+        #endregion
+
+        #region Internal methods
+
+        /// <summary>
+        /// Create from existing Element
+        /// </summary>
+        /// <param name="plan"></param>
+        /// <param name="isRevitOwned"></param>
+        /// <returns></returns>
+        internal static View3DTemplate FromExisting(Autodesk.Revit.DB.View3D plan, bool isRevitOwned)
+        {
+            if (plan == null)
+            {
+                throw new ArgumentNullException("3d_view");
+            }
+
+            return new View3DTemplate(plan)
+            {
+                IsRevitOwned = isRevitOwned
+            };
+        }
+
+        #endregion
+    }
+}
