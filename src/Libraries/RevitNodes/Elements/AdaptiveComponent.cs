9bf355e4eebec186c76580ed49c427a3e47b1fde
Teodor Rujan <teodor.rujan@autodesk.com>
2025-05-28 10:26:57 +0300

    remove using from AdaptiveComponents.cs in order to remove any modification to it

--- src/Libraries/RevitNodes/Elements/AdaptiveComponent.cs
+++ src/Libraries/RevitNodes/Elements/AdaptiveComponent.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Autodesk.DesignScript.Runtime;
 using Autodesk.Revit.Creation;
 using Autodesk.Revit.DB;
-using DynamoServices;
 using Revit.GeometryConversion;
 using Revit.GeometryReferences;
 using RevitServices.Elements;
