5873b8684a7c6002fa7faca248a57b06d1ebdb29
Teodor Rujan <teodor.rujan@autodesk.com>
2025-07-28 09:45:40 +0300

    Modified restorePackages to use net 8.0 as a framework when downloading packages

--- src/restorepackages.bat
+++ src/restorepackages.bat
@@ -26,8 +26,10 @@ set AgetFile=%CurrentDir%\Tools\Aget\aget.exe
 set NugetConfig=%ConfigDir%\dynamo-nuget.config
 
 REM 2. download 3rdParty packages by Aget.exe
+	
+	echo Python Aget:
     echo Running Python script from %AgetFile% using dynamo-nuget.config file
-    set PythonAget="%AgetFile%" -os win -config release -iset intel64 -toolchain v140 -linkage shared -packagesDir "%DynamoPackages%" -nuget "%NugetExe%" -framework NET70 -nugetConfig "%NugetConfig%"
+    set PythonAget="%AgetFile%" -os win -config release -iset intel64 -toolchain v140 -linkage shared -packagesDir "%DynamoPackages%" -nuget "%NugetExe%" -framework NET80 -nugetConfig "%NugetConfig%"   
 
     call :TrackTime "[Aget] Downloading NuGet packages from the NuGet Gallery and the Artifactory server, might take a while if running for the first time."
     echo If any package is not found in the NuGet Gallery, redirect to look up in the Artifactory server...
@@ -36,8 +38,10 @@ REM 2. download 3rdParty packages by Aget.exe
     %PythonAget% -agettable "%ConfigDir%\packages.aget" -refsDir "%SymLinksDir%"
     if ERRORLEVEL 1 (
         echo ERROR: Failed to update Dynamo 3rdParty nuget packages in packages.aget
-        exit /b 1
+		echo CONTINUE DESPITE DOWNLOAD WARNINGS, SOME OLDER PACKAGES GIVE FALSE INCOPABILITY ERRORS WITH NET 8.0
+        REM exit /b 1
     )
+
     call :TrackTime "%~n0: exiting from batch script"
     endlocal
     exit /b 0
