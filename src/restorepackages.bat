4932d8c4ec9a725aee38fbfeb1abe7d88123015b
Teodor Rujan <teodor.rujan@autodesk.com>
2025-08-26 16:51:43 +0300

    Modifications to make 2026 work on local

--- src/restorepackages.bat
+++ src/restorepackages.bat
@@ -15,9 +15,6 @@ REM 1. set variable values
 set DynamoPackages=%~dp0\packages\_packages
 echo %DynamoPackages%
 set CurrentDir=%~dp0
-
-echo Current Dir: %CurrentDir%
-
 if %CurrentDir:~-1%==\ (
     set CurrentDir=%CurrentDir:~0,-1%
 )
@@ -29,10 +26,8 @@ set AgetFile=%CurrentDir%\Tools\Aget\aget.exe
 set NugetConfig=%ConfigDir%\dynamo-nuget.config
 
 REM 2. download 3rdParty packages by Aget.exe
-	
-	echo Python Aget:
     echo Running Python script from %AgetFile% using dynamo-nuget.config file
-    set PythonAget="%AgetFile%" -os win -config release -iset intel64 -toolchain v140 -linkage shared -packagesDir "%DynamoPackages%" -nuget "%NugetExe%" -framework NET80 -nugetConfig "%NugetConfig%"   
+    set PythonAget="%AgetFile%" -os win -config release -iset intel64 -toolchain v140 -linkage shared -packagesDir "%DynamoPackages%" -nuget "%NugetExe%" -framework NET80 -nugetConfig "%NugetConfig%"
 
     call :TrackTime "[Aget] Downloading NuGet packages from the NuGet Gallery and the Artifactory server, might take a while if running for the first time."
     echo If any package is not found in the NuGet Gallery, redirect to look up in the Artifactory server...
@@ -41,10 +36,8 @@ REM 2. download 3rdParty packages by Aget.exe
     %PythonAget% -agettable "%ConfigDir%\packages.aget" -refsDir "%SymLinksDir%"
     if ERRORLEVEL 1 (
         echo ERROR: Failed to update Dynamo 3rdParty nuget packages in packages.aget
-		echo CONTINUE DESPITE DOWNLOAD WARNINGS, SOME OLDER PACKAGES GIVE FALSE INCOPABILITY ERRORS WITH NET 8.0
-        REM exit /b 1
+        exit /b 1
     )
-
     call :TrackTime "%~n0: exiting from batch script"
     endlocal
     exit /b 0
@@ -52,4 +45,4 @@ REM 2. download 3rdParty packages by Aget.exe
 echo ========================================================
 echo %~1
 time /t
-echo ======================================================== 
+echo ========================================================
