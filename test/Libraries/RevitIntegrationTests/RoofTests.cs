8285ab03e85ee34bbacf4c82c13f9d957d66ddc8
Alice Viorela Malina <alice.viorela.malina@autodesk.com>
2025-10-03 15:35:10 +0300

    new tests and changes

--- test/Libraries/RevitIntegrationTests/RoofTests.cs
+++ test/Libraries/RevitIntegrationTests/RoofTests.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.IO;
 using NUnit.Framework;
 using RTF.Framework;
 using RevitTestServices;
+using Dynamo.Graph.Nodes.ZeroTouch;
+using System.Linq;
 
 namespace RevitSystemTests
 {
@@ -26,7 +23,7 @@ namespace RevitSystemTests
 
 
             var type = GetPreviewValue("9daa9b583bdf4659901779b540fdeecc");
-            Assert.AreEqual(type.GetType(), typeof(Revit.Elements.Roof));
+            Assert.AreEqual(typeof(Revit.Elements.Roof), type.GetType());
         }
 
         [Test]
@@ -42,7 +39,69 @@ namespace RevitSystemTests
 
 
             var type = GetPreviewValue("c6d16ef7c551467a8d46cd307b2a465b");
-            Assert.AreEqual(type.GetType(), typeof(Revit.Elements.Roof));
+            Assert.AreEqual(typeof(Revit.Elements.Roof), type.GetType());
+        }
+
+        [Test]
+        [TestModel(@".\DifferentTypeRooms.rvt")]
+        public void RoofAddAndMovePoint()
+        {
+            string samplePath = Path.Combine(workingDirectory, @".\Script\RoofAddAndMovePoint.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+            AssertNoDummyNodes();
+            RunCurrentModel();
+
+            var roofByOutline = GetFlattenedPreviewValues("58315d46b9934fafb62bceaca05b2255");
+            Assert.AreEqual(typeof(Revit.Elements.Roof), roofByOutline[0].GetType());
+
+            var roofPoints1 = AllNodes
+            .FirstOrDefault(n => n.Name == "roof.Points1");
+
+            //var roofPoints1 = "0a224bb8655743fcbb6fb5c1e002bbf0";
+
+            var roofByOutlineNode = GetNode<DSFunction>("58315d46-b993-4faf-b62b-ceaca05b2255");
+            var roofPoints1Node = GetNode<DSFunction>("0a224bb8-6557-43fc-bb6f-b5c1e002bbf0");
+
+            //Connect the output of Roof.ByOutlineTypeAndLevel node to the input of Roof.Points1 node
+            MakeConnector(roofByOutlineNode, roofPoints1Node, 0, 0);
+            RunCurrentModel();
+
+            var roofPoints1Value = GetFlattenedPreviewValues(roofPoints1Node.GUID.ToString("N"));
+            Assert.AreEqual(7, roofPoints1Value.Count);
+            var roofPoints1ExpectedValue = "Point(X = -9976.661, Y = -7119.928, Z = 4350.500)";
+            Assert.AreEqual(roofPoints1ExpectedValue, roofPoints1Value[0].ToString());
+
+            var roofPoints2 = AllNodes
+            .FirstOrDefault(n => n.Name == "roof.Points2");
+            var roofPoints2Node = GetNode<DSFunction>("198154aa-94e4-4857-aa4c-6980dd1eef98");
+
+            //Connect the output of Roof.ByOutlineTypeAndLevel node to the input of Roof.Points2 node
+            MakeConnector(roofByOutlineNode, roofPoints2Node, 0, 0);
+            RunCurrentModel();
+
+            var roofPoints2Value = GetFlattenedPreviewValues(roofPoints2Node.GUID.ToString("N"));
+            Assert.AreEqual(7, roofPoints2Value.Count);
+            var roofPoints2ExpectedValue = "Point(X = -9976.661, Y = -7119.928, Z = 4960.100)";
+            Assert.AreEqual(roofPoints2ExpectedValue, roofPoints2Value[0].ToString());
+        }
+
+        [Test]
+        [TestModel(@".\Revision2025.rvt")]
+        public void RoofByOutlineExtrusionTypeAndLevel()
+        {
+            string samplePath = Path.Combine(workingDirectory, @".\Script\RoofByOutlineExtrusionTypeAndLevel.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            ViewModel.OpenCommand.Execute(testPath);
+            RunCurrentModel();
+
+            var roofByOutline = GetPreviewValue("541167eee9a9407faf6e036fecbc61c0");
+            Assert.AreEqual(typeof(Revit.Elements.Roof), roofByOutline.GetType());
+
+            var roofTypeName = GetPreviewValue("495506b35585413389c1486863480f32");
+            Assert.AreEqual("Roof_Generic-400mm", roofTypeName);
         }
     }
 }
